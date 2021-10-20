98cea7299618b7a447428c7621ee37962d28a8a7
Ami Berman <aberman@yext.com>
2021-10-20 13:23:56 -0400

    Add new fields + transform

---
+++ platform/plugins/stockStatusConverter/mod.ts
@@ -0,0 +1,9 @@
+export function convertStockStatus(availability: string) {
+    if (availability === 'in stock') {
+        return "IN_STOCK";
+    } else if (availability === 'out of stock' {
+        return "OUT_OF_STOCK";
+    } else if (availability === 'preorder') {
+        return "PRE_ORDER";
+    }
+}
